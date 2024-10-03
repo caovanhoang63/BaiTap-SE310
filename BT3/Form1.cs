bd6ef58fca1e059483c6a406d3c375109854400f
caovanhoang63 <22520457@gm.uit.edu.vn>
2024-10-03 21:31:10 +0700

    init bt3

---
+++ BT3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT3
+{
+  public partial class Form1 : Form
+    {
+        private readonly List<Product> _products = new List<Product>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProduct(object sender, EventArgs e)
+        {
+            var p = new Product(textBox1.Text,textBox2.Text,int.Parse(textBox3.Text),decimal.Parse(textBox4.Text),  textBox5.Text,dateTimePicker1.Value);
+            _products.Add(p);
+        }
+
+
+        private void GetHighestPrice(object sender, EventArgs e)
+        {
+            
+            var mostExpensive = _products.OrderByDescending(p => p.Price).FirstOrDefault();
+            var source = new BindingSource();
+            source.DataSource = mostExpensive;
+            dataGridView1.DataSource = source;
+        }
+
+
+
+    }
+}
